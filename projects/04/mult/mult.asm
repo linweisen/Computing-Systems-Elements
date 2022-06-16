5b21ef08f58f330c012a848d6064d57e4e307c00
linweisen <linws.sai@gmail.com>
2022-06-16 17:31:39 +0800

    优化project01

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0    //R2 = 0
+@1
+D=M    //D = R0
+@END
+D;JEQ  //if R1 == 0 goto end
+@0
+D=M
+@END
+D;JEQ  //if R0 ==0 goto end
+(LOOP)
+@0
+D=M
+@2
+M=M+D  //R2 = R0 + R2
+@1
+M=M-1
+D=M
+@LOOP
+D;JGT
+(END)
+@END
+0;JMP
